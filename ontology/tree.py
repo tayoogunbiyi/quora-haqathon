d0ed5d7d68ac99b125c283bdbaf4e7b59e4073f4
eyitayo <eyitayoogunbiyi@gmail.com>
2021-03-28 16:58:47 +0100

    made some changes to tree parsing logic

--- ontology/tree.py
+++ ontology/tree.py
@@ -21,21 +21,17 @@ def parse_topics_into_tree_util(topic_list: List[str],processed: set) -> List[Tr
 
     def has_children_topics(idx):
         if idx == len(topic_list)-1:
-            return True
-        return topic_list[idx+1] != "("
+            return False
+        for i in range(idx+1,len(topic_list)):
+            if topic_list[i] != " ":
+                return topic_list[i] == "("
 
-    i = 0
     for i in range(len(topic_list)):
         if topic_list[i] not in processed:
             processed.add(topic_list[i])
             next_node = TreeNode(topic_list[i])
-
-            if not has_children_topics(i):
-                new_topic_list = topic_list[i+2:-1]
-                cc = parse_topics_into_tree_util(new_topic_list,processed)
-                for child_topic in cc:
-                    next_node.children.append(child_topic)
-            
+            if has_children_topics(i):
+                next_node.children = parse_topics_into_tree_util(topic_list[i+2:-1],processed)
             res.append(next_node)
     return res
 
@@ -45,11 +41,14 @@ def parse_topics_into_tree(topics: str) -> TreeNode:
         return None
 
     topic_list = topics.split(" ")
+    if len(topic_list) == 0:
+        return None
+    
     root = TreeNode(topic_list[0])
     processed = set()
     processed.add("(")
     processed.add(")")
-
+    
     for child_topic in parse_topics_into_tree_util(topic_list[2:-1],processed):
         root.children.append(child_topic)
     return root
